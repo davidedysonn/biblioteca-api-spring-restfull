899ffcf1758ee339b23cf332d9f758eeaf7f7ddd
David Belfort <david_edysonn@hotmail.com>
2024-01-17 10:24:41 -0300

    Projeto inicial com suas dependencias.

---
+++ restfull-with-spring-crud-completo-pagination/src/main/java/br/com/biblioteca/restfullwithspringcrudcompletopagination/RestfullWithSpringCrudCompletoPaginationApplication.java
@@ -0,0 +1,13 @@
+package br.com.biblioteca.restfullwithspringcrudcompletopagination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfullWithSpringCrudCompletoPaginationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfullWithSpringCrudCompletoPaginationApplication.class, args);
+	}
+
+}
